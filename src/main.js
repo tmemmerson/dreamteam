ef5371000eaf933f8f4de9ca8baba13921d62f2b
Tristan Emmerson <tristan@stickerslug.com>
2020-07-06 10:03:26 -0700

    reset.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,3 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
-$(document).ready(function () {
-  // $('#submit-button').submit(function (event) {
-  //     event.preventDefault();
-
-  // });
-});
