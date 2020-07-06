e84faea3bb6cbd6459105cf0acb47da361d0ca80
Tristan Emmerson <tristan@stickerslug.com>
2020-07-06 13:38:00 -0700

    establish first API call iteration.
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>
    Co-authored-by: Teresa Rosinski  <trosinski89@gmail.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>

---
+++ src/call.js
@@ -0,0 +1,16 @@
+export  async function getPlayer () {
+  try{
+    let response = await fetch (`https://www.balldontlie.io/api/v1/players/`)
+    let playerData;
+    if (response.ok && response.status == 200){
+      playerData = await response.json();
+    }else {
+      playerData = false;
+      console.log(`response: ${response}`);
+    }
+    return playerData;
+  }catch(error){
+    console.log(`error: ${error}`);
+    return false;
+  }
+}
