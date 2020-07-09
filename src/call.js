3ac37c384b1ecccb7d1be5326cf5d7ba344507f1
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-09 13:37:41 -0500

    add functionality to show player names and stats

--- src/call.js
+++ src/call.js
@@ -1,86 +1,93 @@
-/*
-export  async function getPlayerByName (playerName) {
+export async function getPlayerNameIdPos (name) {
   try{
-    let response = await fetch (`https://www.balldontlie.io/api/v1/players/?search=${playerName}`);
+    let currentPage = 0;
     let playerData;
-    let playerIds;
-    if (response.ok && response.status == 200){
-      playerData = await response.json();
-      console.log(playerData);
-      console.log(playerData.data);
-      playerIds = playerData.data.map(player => {
-        console.log(player.id);
-        return player.id;
-      });
-      console.log(playerIds);
-    }else {
-      playerData = false;
-      console.log(`response: ${response}`);
-    }
-    return playerData.data;
-  }catch(error){
-    console.log(`error: ${error}`);
-    return false;
-  }
-}
-*/
-export async function getPlayerNameIdPos () {
-  try{
-    let response = await fetch (`https://www.balldontlie.io/api/v1/players?per_page=100&page=0`);
-    let playerData;
-    let playerIdsAndNames =[];
-    if (response.ok && response.status == 200){
-      playerData = await response.json();
-      //console.log(playerData);
-      //console.log(playerData.data);
-      playerIdsAndNames = playerData.data.map(player => {
-        const newPlayer = {
-          Id: player.id, 
-          first_name: player.first_name, 
-          last_name: player.last_name,
-          position: player.position,
-          team_name: player.team.abbreviation,
-          team_Id: player.team.id
-        };
-        //console.log(playerIdsAndNames);
-        return newPlayer;
-      });
-
-      console.log(playerIdsAndNames);
-    }else {
-      playerData = false;
-      console.log(`response: ${response}`);
-    }
-    return playerData.data;
+    let playerIdsAndNames = [];
+    do {
+      let response = await fetch (`https://www.balldontlie.io/api/v1/players?search=${name}&per_page=100&page=${currentPage}`);
+      currentPage++;
+      if (response.ok && response.status == 200){
+        playerData = await response.json();
+        playerData.data.forEach(player => {
+          const newPlayer = {
+            Id: player.id, 
+            first_name: player.first_name, 
+            last_name: player.last_name,
+            position: player.position,
+            team_name: player.team.abbreviation,
+            team_Id: player.team.id
+          };
+          playerIdsAndNames.push(newPlayer);
+        });
+      } else {
+        playerData = false;
+        console.log(`response: ${response}`);
+      }
+    } while (playerData.meta.next_page);
+    return playerIdsAndNames;
   }catch(error){
     console.log(`error: ${error}`);
     return false;
   }
 }
 
-console.log(getPlayerNameIdPos());
-
-/*
-export async function getStatsForPlayer(await getAllPlayers(playerName)){
-  try{
-  let statsResponse = await fetch (`https://www.balldontlie.io/api/v1/stats?player_ids[]=${playerIds}`);
-  let playerStats;
-  if(statsResponse.ok && statsResponse == 200) {
-    playerStats = await statsResponse.json();
-    console.log(playerStats)
+export async function getPlayerStats (Id) {
+  try {
+    let playerStats;
+    let stats;
+        let response = await fetch (`https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=${Id}`);
+        if (response.ok && response.status == 200) {
+          stats = await response.json();
+          playerStats = {
+            points: stats.data[0].pts,
+            assists: stats.data[0].ast,
+            rebounds: stats.data[0].reb
+          }
+        console.log(playerStats);
+        } else {
+          stats = false;
+          console.log(`response: ${response}`);
+        }
     return playerStats;
-  } else {
-    playerStats = false; 
-    console.log(statsResponse);
-  }
-  return playerStats;
-  }catch(error){
-  console.log(`error: ${error}`);
-  return false;
+  } catch(error) {
+    console.log(`Uh oh, something bad happened: ${error}`);
+    return false;
   }
 }
 
-console.log(getStatsForPlayer(await getAllPlayers('Lebron')));
-*/
-//Now we have an array of players and we need to display the player name, id, and team to the webpage
 
+
+export async function getCurrentPlayers (name){
+  let allPlayers = await getPlayerNameIdPos(name); // an array of objects 
+  console.log(allPlayers);
+  let currentPlayers = [];
+  allPlayers.forEach(function(player) {
+    if (player.position !== "") {
+      currentPlayers.push(player);
+    }
+  });
+  console.log(currentPlayers);
+  return currentPlayers;
+}
+
+// export function sortPosition() {
+  // let forwards = [];
+  // let guards = [];
+  // let centers=  [];
+  // allPlayers.forEach(function(player) {
+    // if (player.position === 'F' || player.position === 'F-C' || player.position === 'F-G' || player.position === 'G-F' || player.position === 'C-F') {
+    //   forwards.push(player);
+    // }
+    // if (player.position === 'G' || player.position === 'G-C' || player.position === 'F-G' || player.position === 'G-F' || player.position === 'C-G') {
+    //   guards.push(player);
+    // }
+    // if (player.position === 'C' || player.position === 'F-C' || player.position === 'C-G' || player.position === 'G-C' || player.position === 'C-F') {
+    //   centers.push(player);
+  // }
+  // }
+  // return {
+  //   forwards: forwards, 
+  //   guards: guards, 
+  //   centers: centers
+  // }
+// }
--- src/main.js
+++ src/main.js
@@ -2,34 +2,42 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {getAllPlayers} from './call.js';
-
-/*
-players.data = [
-  {
-    id: 14,
-  },
-  {
-    id: 15
-  }
-]
-
-const searchedPlayer = players.data.find(player => {
-  player.id === 14
-})
-*/ 
-/*
-async function parseData() {
-  const playerData = await getAllPlayers(); // calls all the player data
-  if(playerData === false) {
-    $("#printStat").Text("I'm sorry there was an error with your request.");
-  } else {
-    $("#printStat").text(`here is your stat ${playerData.data[0].id}`);
-  }
-}
-*/
+import { getCurrentPlayers } from './call.js';
+import { getPlayerStats } from './call.js';
 
 $(document).ready(function(){
-  $
+  $("#formSearchPlayerName").submit(function(event) {
+    event.preventDefault();
+    const name = $("input#playerNameSearch").val();
+    
+    async function apiCall() {
+      
+      const currentPlayers = await getCurrentPlayers(name);
+          for (let i = 0; i < currentPlayers.length; i++) {
+        $("#showPlayers").append('<li>' + currentPlayers[i].first_name + " " + currentPlayers[i].last_name + " - Id Number " + currentPlayers[i].Id + '</li>');
+      console.log(currentPlayers);
+      }
+      
+      return false;
+    }
+    apiCall();
+
+    $("#showStatsByPlayerId").submit(function(event) {
+      event.preventDefault();
+      const ID = $("input#IdNumber").val();
+      async function apiCallStats() {
+        // retrieves stats based on ID entered
+        const playerStats = await getPlayerStats(ID);
+        $('#printStat').text("Points: " + playerStats.points + " Assist: " + playerStats.assists + " Rebounds" + playerStats.rebounds);
+      }
+      apiCallStats();
+    });
+    
+  });
 });
 
+async function doThing (){
+console.log(await getPlayerStats(237));
+}
+
+doThing(); 
